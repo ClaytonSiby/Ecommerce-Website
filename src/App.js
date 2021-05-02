8d0824a3ed8867b12a82e7f010ebe78b8e26d7f5
ClaytonSiby <claytonsiby@gmail.com>
2021-05-02 22:26:22 +0200

    Add the Routing feature to switch between the Homepage & Registration components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Homepage from './pages/Homepage'
+import Registration from './pages/Registration'
 import './default.scss'
 
 function App () {
   return (
-    <div className='App'>
+    <Container className='App'>
       <Header />
       <div className='main'>
-        <Homepage />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/registration' component={Registration} />
+        </Switch>
       </div>
-    </div>
+    </Container>
   )
 }
 
---
+++ src/pages/Registration/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './style.scss'
+
+class Registration extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Registration</h1>
+      </div>
+    )
+  }
+}
+
+export default Registration
