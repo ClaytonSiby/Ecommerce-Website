d10ee0bba110e71daca743e9c5aded8344b6f10c
ClaytonSiby <claytonsiby@gmail.com>
2021-05-02 11:58:21 +0200

    Configure github actions & eslint, initialize the header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import React from 'react';
+import Header from './components/header';
+import './default.scss';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello World</h1>
     </div>
   );
