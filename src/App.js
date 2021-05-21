a6e9889d4964ddbaf5d1120c4624e100c113863d
ClaytonSiby <claytonsiby@gmail.com>
2021-05-21 20:57:33 +0200

    Add ES class lifecycle hooks to accomodate authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 // components
 import { Route, Switch } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+import { auth } from './Firebase/utils';
 
 // layouts
 import MainLayout from './Layouts/MainLayout'
@@ -16,38 +17,68 @@ import Login from './pages/Login'
 // styles
 import './default.scss'
 
-function App () {
-  return (
-    <Container className='App'>
-      <Switch>
-        <Route
-          exact
-          path='/'
-          render={() => (
-            <HomepageLayout>
-              <Homepage />
-            </HomepageLayout>
-          )}
-        />
-        <Route
-          path='/registration'
-          render={() => (
-            <MainLayout>
-              <Registration />
-            </MainLayout>
-          )}
-        />
-        <Route
-          path='/login'
-          render={() => (
-            <MainLayout>
-              <Login />
-            </MainLayout>
-          )}
-        />
-      </Switch>
-    </Container>
-  )
+const initialState = {
+  currentUser: null
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ...initialState
+    }
+  }
+
+  authListener = null;
+
+  componentDidMount() {
+    this.authListener = auth.onAuthStateChanged(userAuth => {
+      if(!userAuth) return;
+
+      this.setState({
+        currentUser: userAuth
+      });
+    })
+  }
+
+  componentWillUnmount() {
+    this.authListener();
+  }
+
+  render() {
+    return (
+      <Container className='App'>
+        <Switch>
+          <Route
+            exact
+            path='/'
+            render={() => (
+              <HomepageLayout>
+                <Homepage />
+              </HomepageLayout>
+            )}
+          />
+          <Route
+            path='/registration'
+            render={() => (
+              <MainLayout>
+                <Registration />
+              </MainLayout>
+            )}
+          />
+          <Route
+            path='/login'
+            render={() => (
+              <MainLayout>
+                <Login />
+              </MainLayout>
+            )}
+          />
+        </Switch>
+      </Container>
+    )
+  }
 }
 
 export default App
