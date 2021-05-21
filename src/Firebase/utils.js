40f45d46fb59fa5c9c100b7d32e4bc3104e8c420
ClaytonSiby <claytonsiby@gmail.com>
2021-05-21 12:43:51 +0200

    Add the authentication feature to singin users using google service provider

--- src/Firebase/utils.js
+++ src/Firebase/utils.js
@@ -2,3 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import { firebaseConfig} from './config';
+
+firebase.initializeApp(firebaseConfig);
+
+// utilities used across our application authentication
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const GoogleProvider = new firebase.auth.GoogleAuthProvider();
+GoogleProvider.setCustomParameters({ prompts: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(GoogleProvider);
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from './../Forms/Button';
+import { signInWithGoogle } from './../../Firebase/utils';
 import './style.scss';
 
-const SignIn = () => {
-    return (
-        <div className="signIn">
-            <div className="wrap">
-                <h2>LogIn</h2>
+class SignIn extends Component {
+    handleSubmit = async e => {
+        e.preventDefault();
+    }
+    render() {
+        return (
+            <div className="signIn">
+                <div className="wrap">
+                    <h2>LogIn</h2>
+    
+                    <div className="formWrap">
+                        <form onSubmit={ this.handleSubmit }>
+                            <div className="socialSignin">
+                                <div className="row my-2">
+                                    <Button onClick={signInWithGoogle}>Sign in with Google</Button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default SignIn;
