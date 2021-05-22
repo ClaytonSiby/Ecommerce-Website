7d7f4cf37eff4752c73fdb4d36e266bb9fd1f8be
ClaytonSiby <claytonsiby@gmail.com>
2021-05-22 20:13:09 +0200

    Add the Signup page & its corresponding styles

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,13 +1,79 @@
 import React, { Component } from 'react'
-import './styles.scss';
+import FormInput from './../Forms/FormInput'
+import Button from './../Forms/Button'
+import './styles.scss'
+
+const initialState = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+}
 
 class SignUp extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      ...initialState
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    const { name, value } = e.target
+
+    this.setState({
+      [name]: value
+    })
+  }
+
   render () {
+    const { displayName, email, password, confirmPassword } = this.state
     return (
       <div className='signup'>
         <div className='wrap'>
           <h2>SignUp</h2>
-          <form></form>
+          <div className="formWrap">
+          <form>
+            <FormInput
+              type='text'
+              name='displayName'
+              value={displayName}
+              onChange={this.handleChange}
+              placeholder='Full Name'
+            />
+
+            <FormInput
+              type='email'
+              name='password'
+              value={email}
+              onChange={this.handleChange}
+              placeholder='Email Address'
+            />
+
+            <FormInput
+              type='password'
+              name='password'
+              value={password}
+              onChange={this.handleChange}
+              placeholder='********'
+            />
+
+            <FormInput
+              type='password'
+              name='password'
+              value={confirmPassword}
+              onChange={this.handleChange}
+              placeholder='********'
+            />
+
+            <Button type="submit">
+              Register
+            </Button>
+          </form>
+          </div>
         </div>
       </div>
     )
