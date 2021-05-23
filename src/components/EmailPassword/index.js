bc2d5cb05af03679ed43c3386dcb541bd2d7dd19
ClaytonSiby <claytonsiby@gmail.com>
2021-05-23 19:14:02 +0200

    Setup the EmailPassword component

--- src/components/EmailPassword/index.js
+++ src/components/EmailPassword/index.js
@@ -1,23 +1,64 @@
-import React, { Component } from 'react';
-import './styles.scss';
+import React, { Component } from 'react'
+import './styles.scss'
 
-import AuthWrapper from './../AuthWrapper';
-import FormInput from './../Forms/FormInput';
-import Button from './../Forms/Button';
+import AuthWrapper from './../AuthWrapper'
+import FormInput from './../Forms/FormInput'
+import Button from './../Forms/Button'
 
-class EmailPassword extends Component {
-
-    render() {
-
-        const configAuthWrapper = {
-            headline: 'Email Password'
-        }
-        return (
-            <AuthWrapper { ...configAuthWrapper }>
-
-            </AuthWrapper>
-        );
-    }
+const initialState = {
+  email: ''
 }
 
-export default EmailPassword;
+class EmailPassword extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      ...initialState
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (event) {
+    const { name, value } = event.target;
+
+    this.setState({
+        [name]: value
+    })
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    
+  }
+
+  render () {
+    const configAuthWrapper = {
+      headline: 'Email Password'
+    }
+
+    const { email } = this.state
+
+    return (
+      <AuthWrapper {...configAuthWrapper}>
+        <div className='formWrap'>
+          <form onSubmit={this.handleSubmit}>
+            <FormInput
+              type='email'
+              name='email'
+              value={email}
+              placeholder='Email Address'
+              onChange={this.handleChange}
+            />
+
+            <Button type="submit">Email Password</Button>
+          </form>
+        </div>
+      </AuthWrapper>
+    )
+  }
+}
+
+export default EmailPassword
