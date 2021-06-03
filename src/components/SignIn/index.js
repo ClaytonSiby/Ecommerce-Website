f617738e214855deb3cfd1a6404cf1951b1c7edd
ClaytonSiby <claytonsiby@gmail.com>
2021-06-03 21:19:44 +0200

    Refactor the Signin class component to use a functional component

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../Forms/Button';
 import { signInWithGoogle, auth } from '../../Firebase/utils';
@@ -6,75 +6,63 @@ import FormInput from '../Forms/FormInput';
 import AuthWrapper from '../AuthWrapper';
 import './style.scss';
 
-const initialState = {
-  email: '',
-  password: '',
-};
+const SignIn = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-class SignIn extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      ...initialState,
-    };
-
-    this.handleChange = this.handleChange.bind(this);
+  const resetForm = () => {
+    setEmail('');
+    setPassword('');
   }
 
-    handleSubmit = async (e) => {
-      e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-      const { email, password } = this.state;
+		try {
+			// try to signin with provided email and password
+			await auth.signInWithEmailAndPassword(email, password);
+			// reset the form
+      resetForm();
+		} catch (error) {
+			// console.log(error);
+		}
+	};
 
-      try {
-        // try to signin with provided email and password
-        await auth.signInWithEmailAndPassword(email, password);
+	const configAuthWrapper = {
+		headline: 'LogIn',
+	};
 
-        // reset the form
-
-        this.setState({
-          ...initialState,
-        });
-      } catch (error) {
-        // console.log(error);
-      }
-    }
-
-    handleChange(e) {
-      const { value, name } = e.target;
-
-      this.setState({
-        [name]: value,
-      });
-    }
-
-    render() {
-      const { email, password } = this.state;
-      const configAuthWrapper = {
-        headline: 'LogIn',
-      };
-
-      return (
-        <AuthWrapper {...configAuthWrapper}>
-          <div className="formWrap">
-            <form onSubmit={this.handleSubmit}>
-              <FormInput type="emial" value={email} name="email" onChange={this.handleChange} placeholder="email address" />
-              <FormInput type="password" value={password} name="password" onChange={this.handleChange} placeholder="password" />
-              <Button type="submit">Login</Button>
-              <div className="socialSignin">
-                <div className="">
-                  <Button onClick={signInWithGoogle}>Sign in with Google</Button>
-                </div>
-              </div>
-              <div className="links">
-                <Link to="/recovery">Reset Password</Link>
-              </div>
-            </form>
-          </div>
-        </AuthWrapper>
-      );
-    }
-}
+	return (
+		<AuthWrapper {...configAuthWrapper}>
+			<div className="formWrap">
+				<form onSubmit={handleSubmit}>
+					<FormInput
+						type="emial"
+						value={email}
+						name="email"
+						onChange={e => setEmail(e.target.value)}
+						placeholder="email address"
+					/>
+					<FormInput
+						type="password"
+						value={password}
+						name="password"
+						onChange={e => setPassword(e.target.value)}
+						placeholder="password"
+					/>
+					<Button type="submit">Login</Button>
+					<div className="socialSignin">
+						<div className="">
+							<Button onClick={signInWithGoogle}>Sign in with Google</Button>
+						</div>
+					</div>
+					<div className="links">
+						<Link to="/recovery">Reset Password</Link>
+					</div>
+				</form>
+			</div>
+		</AuthWrapper>
+	);
+};
 
 export default SignIn;
